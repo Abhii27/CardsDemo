a3638cb13a9608fadda157b64f02d3f6c0346ffa
Abhii27 <abhishekpanchal@1998>
2022-01-09 18:03:19 +0530

    Cards added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/images/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },
+    {
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/images/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Here is a picture of a snowy Mountain'
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl: 'assets/images/biking.jpeg',
+      username: 'biking11',
+      content: 'I did some biking today'
+    },
+    {
+      title: 'Neat Tree',
+      imageUrl: 'assets/images/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },{
+      title: 'Snowy Mountain',
+      imageUrl: 'assets/images/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Here is a picture of a snowy Mountain'
+    },
+    {
+      title: 'Mountain Biking',
+      imageUrl: 'assets/images/biking.jpeg',
+      username: 'biking11',
+      content: 'I did some biking today'
+    }
+    
+  ];
 }
